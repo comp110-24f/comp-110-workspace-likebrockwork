95f69ca3bdd7ae9465828d02a03cb2f71d210b3a
brock <jbrocks@unc.edu>
2024-08-23 11:55:25 -0400

    Finished Exercise 00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,10 @@
+"""My first excercise in COMP110!"""
+
+__author__ = "730652013"
+
+print("Hello, world!")
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
